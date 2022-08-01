6652bb253d81ed0203b817a3e5899d9246c06c3c
+ <+>
2022-08-01 16:47:21 -0400

    render Pokemon

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 // display/profile pic
-console.log('hello')
+let pokemonArray = [1,4,7,25];
+let evolvedArray = [2,3,5,6,8,9];
+
+pokemonArray.forEach(pokeId => renderPokemon(pokeId));
+
+function renderPokemon(id){
+        fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+        .then(res => res.json())
+        .then(pokemon => {;
+           // console.log(pokemon.sprites.front_default);
+            newPokemon = document.createElement("div");
+            newPokemon.className = "card";
+            let name = document.createElement("h2");
+            name.textContent = pokemon.name;
+            let image = document.createElement("img");
+            image.src = pokemon.sprites.front_default;
+            newPokemon.append(name,image);
+            
+            document.getElementById("pok").append(newPokemon);
+  });
+}
